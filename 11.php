4234734206183f29f384585b4097b1b013f897e0
Aman <amanali7239@gmail.com>
2023-03-24 10:21:34 +0530

    added question in 11th program

--- 11.php
+++ 11.php
@@ -1,3 +1,5 @@
+<!-- 11. Write a PHP Script for establish connection using localhost and provide details that connection
+established or not -->
 <?php
 $conn=mysqli_connect('localhost','root','');
 $conn?print("Connected to db"):print("Failed to connect");
